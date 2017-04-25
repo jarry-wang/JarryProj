3179b19f74b1f916f9eb0375eca8e4a17f82accc
Jarry-wang <407506152@qq.com>
2017-04-26 00:52:54 +0800

    1.1
    1.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,7 +11,11 @@ Page({
         images: [],
         imageUrls:[],
         col1: [],
-        col2: []
+        col2: [],
+        poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+        name: '特别的人',
+        author: '方大同',
+        src: 'http://7xjope.com1.z0.glb.clouddn.com/%E7%89%B9%E5%88%AB%E7%9A%84%E4%BA%BA%20-%20%E6%96%B9%E5%A4%A7%E5%90%8C.mp3'
     },
 
     onLoad: function () {
@@ -31,10 +35,26 @@ Page({
                 this.loadImages();
             }
         })
+        this.audioCtx = wx.createAudioContext('myAudio')
     },
     onReady: function( e ) {
-        wx.setNavigationBarTitle( { title: "哎哟不错哦" });
+        // wx.setNavigationBarTitle( { title: "哎哟不错哦" });
+         console.log("onReady");
+    
+    // this.audioCtx.play()
+  
     },
+    onShow: function() {
+    // Do something when page show.
+        console.log("onShow");
+        this.audioCtx.play()
+    },
+    onHide: function() {
+        // Do something when page hide.
+        console.log("onHide");
+        this.audioCtx.pause()
+    },
+
 
      refresh: function () {
         console.log("refresh");
@@ -47,14 +67,9 @@ Page({
         });  
         col1H = 0;
         col2H = 0;
-        var that = this;
-        // that.loadImages();
+        this.loadImages();
      },
 
-    onPullDownRefresh: function() {
-    // Do something when pull down.
-     console.log('刷新');
-    },
     onImageLoad: function (e) {
         console.log("onImageLoad");
         let imageId = e.currentTarget.id;
@@ -106,8 +121,9 @@ Page({
       console.log("loadImages");
         let images = [];
         let imageUrls = [];
-        var Image = Bmob.Object.extend("Image");
+        var Image = Bmob.Object.extend("pic");
         var query = new Bmob.Query(Image);
+        query.descending("createdAt");
         // 查询所有数据
         query.find({
             success: function(results) {
@@ -139,8 +155,8 @@ Page({
     },
 
     seePic: function(event) {
-      console.log("查看图片:"+that.data.imageUrls);
-       var url = event.currentTarget.dataset.picurl;
+        console.log("查看图片:"+that.data.imageUrls);
+        var url = event.currentTarget.dataset.picurl;
         console.log("查看图片:"+url);
         wx.previewImage({
         current: url, // 当前显示图片的http链接
